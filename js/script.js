c6eae005fdbc6ec5199a19200ffcc245c31c1737
Vital <vitalmaloy@gmail.com>
2021-02-03 15:32:06 +0300

    up to date all information

--- js/script.js
+++ js/script.js
@@ -4,6 +4,29 @@ var mySwiper = new Swiper('.swiper-container', {
     
     slidesPerView: 3,
     spaceBetween: 20,
+
+    // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    340: {
+      slidesPerView: 1,
+      spaceBetween: 6,
+      // centeredSlidesBounds: true,
+      centerInsufficientSlides: true,
+      // autoHeight: true,
+      width: 300
+    },
+    // when window width is >= 480px
+    // 480: {
+    //   slidesPerView: 1,
+    //   spaceBetween: 6
+    // },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  },
     // Optional parameters
     // direction: 'vertical',
     // loop: true,
